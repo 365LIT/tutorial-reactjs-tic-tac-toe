934a0fc33c896c70f08c87388a746eccaf8abb48
Woo Hyeon Her <herparousia@gmail.com>
2023-09-27 22:43:53 -0400

    Update Square.tsx

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,3 +1,27 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React from 'react';
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
