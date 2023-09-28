83fd119e1dbee892ce301c1d4a8c2a1d902af6c5
Woo Hyeon Her <herparousia@gmail.com>
2023-09-28 17:50:07 -0400

    Some changes

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,3 +1,23 @@
-export default function Square({ value }: { value: number }) {
+import { useState } from 'react';
+
+
+
+// export default function Square({ value, onSquareClick }: { value: string; onSquareClick: () => void;}) {
+//   const [value, setValue] = useState<String | null> (null);
+//   function handleClick() {
+//     setValue('X');
+//   }
+
+//   return (
+//     <button 
+//     className="square"
+//     onClick={handleClick}
+//     >
+//       {value}
+//     </button>
+//   );
+// }
+
+export default function Square({value}: {value: string}) {
   return <button className="square">{value}</button>;
 }
